c3dd8d5573af8d5b514bc60e1d6c5e5c620360d2
Smokekills123 <nathanielf@filamer.edu.ph>
2025-03-19 23:25:33 -0700

    battleship v_2

--- battleship/battleship_v2.js
+++ battleship/battleship_v2.js
@@ -1,6 +1,10 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+// console.log(Math.floor(math.random() * 5))
+
+// var randomloc = Math.floor(math.random() * 5)
+
+var location1 = randomloc;
+var location2 = location1 + 1;
+var location3 = location2 + 2;
 
 var guess;
 var hits = 0;
@@ -10,17 +14,24 @@ var isSunk = false;
 
 // GAME LOGIC
 
-while (isSunk == false) {
+while (!isSunk) {
     guess = prompt ("Ready, Aim, Fire! (Enter number 0-6)");
+
+if (guess == null) {
+    alert("Thankyou for playing!");
+    break;
+}
 
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!")
+    
+    
     }else {
         guesses = guesses + 1;
 
         if(guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
-            alert("hit!")
+            alert("Hit!")
 
             if (hits == 3) {
                 isSunk = true;
@@ -32,5 +43,8 @@ while (isSunk == false) {
     }
 }
 
-var stats = "You sank my battleship at " + guesses + "guesses," + "and Your Accuracy is; " + (3/guesses);
+var accuracy = (guesses > 0? (hits / guesses)* 100 : 0).toFixed(2)
+
+var stats = "You took" + guesses + "guesses to sink the battleship! ," + " \n " +  "Accuracy:  " + accuracy + "%";
+
 alert(stats);
