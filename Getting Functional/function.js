a3866f73aad0825b06e57d5785d17a46344af74d
Smokekills123 <nathanielf@filamer.edu.ph>
2025-04-20 23:31:24 -0700

    function.js

---
+++ Getting Functional/function.js
@@ -0,0 +1,33 @@
+var dogName = "Coco"
+var dogWeight = 45;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");    
+} else {
+    console.log(dogName + " says arff arff")
+}
+
+var dogName = "MawMaw"
+var dogWeight = 19;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");    
+} else {
+    console.log(dogName + " says arff arff")
+}
+
+
+var dogName = "TamTam"
+var dogWeight = 25;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");    
+} else {
+    console.log(dogName + " says arff arff")
+}
+
+
+var dogName = "potchi"
+var dogWeight = 35;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");    
+} else {
+    console.log(dogName + " says arff arff")
+}
